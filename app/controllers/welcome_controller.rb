03d2da979cdc0b41549c0b28fd43b69cfd7e24ee
Michael Mitchell <mmitchell@uflavor.com>
2012-12-11 17:26:09 -0500

    add Food model and list todays food on mainpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @foods = Food.today
   end
 end
---
+++ app/models/food.rb
@@ -0,0 +1,9 @@
+class Food < ActiveRecord::Base
+  attr_accessible :calories, :name
+
+  scope :today, lambda { 
+    {
+      :conditions => ["created_at >= ?", Time.now.beginning_of_day]
+    }
+  }
+end
