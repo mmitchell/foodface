7fd10987e6a50e47399d1e19fea56b74b297f43c
Michael Mitchell <mmitchell@uflavor.com>
2012-12-11 16:34:37 -0500

    inital commit -> rails new foodface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodface::Application.config.secret_token = 'bf452be8c594048b9db19f5bdabbfbc1dc49bf722b824191a882e8799014e6f05c59421756070b86e108a5afa945f8a505ada6251ab1f8901bdbed0c6a312d64'
