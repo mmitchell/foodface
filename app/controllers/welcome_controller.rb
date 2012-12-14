0d7ce398b2764443cdf71146770c5c008f2c4786
Michael Mitchell <mmitchell@uflavor.com>
2012-12-13 21:54:20 -0500

    add food totaling function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @new_food = Food.new
-    @foods = Food.today.order("created_at desc")
+    @foods = Food.today.order("created_at desc").all
+    @totals = Food.past_day_totals(14)
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,10 @@
 class Food < ActiveRecord::Base
   attr_accessible :calories, :name
-  
+
+  def self.past_day_totals(number_of_days = 1)
+    (1..number_of_days).inject([]) do |totals, days_ago|
+      totals[days_ago-1] = Food.by_day(days_ago.days.ago).sum(:calories)
+      totals
+    end
+  end  
 end
