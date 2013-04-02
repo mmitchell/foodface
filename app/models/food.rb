077126361856d164af1f77d6094fa04df9a1a2c5
Michael Mitchell <mmitchell@uflavor.com>
2013-04-02 15:01:16 -0400

    change calories validation to >= 0

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ActiveRecord::Base
   attr_accessible :calories, :name
   validates :name, :presence => true
-  validates :calories, :numericality => { :only_integer => true, :greater_than => 100 }
+  validates :calories, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
 
   def self.past_day_totals(number_of_days = 1)
     a =(1..number_of_days).inject([[nil],[nil]]) do |arr, idx|
