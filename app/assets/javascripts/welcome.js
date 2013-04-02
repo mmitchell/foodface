a1d3e5255db09f2a4d3c34b8529fd1e52aa1695e
Michael Mitchell <mmitchell@uflavor.com>
2013-04-02 18:06:22 -0400

    add weight data to welcome#index chart and welcome#show page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,21 +7,53 @@ $(function () {
         	},
             chart: {
                 renderTo: 'container',
-                type: 'line'
+                type: 'line',
+                alignTicks: true
             },
             title: {
-                text: 'Calories per day for the last 2 weeks'
+                text: 'Last 2 weeks'
             },
             xAxis: {
                 categories: gon.past_calorie_data[1]
             },
-            yAxis: {
-            	min: 0,
-            	max: 4000,
+            yAxis: [{ // Primary yAxis
+                endOnTick: false,
+                startOnTick: false,
+                min: 0000,
+                max: 4000,
                 title: {
                     text: 'Calories',
-                }
-            },
+                    style: {
+                        color: '#4572A7'
+                    }
+                }, 
+                labels: {
+                    formatter: function() {
+                        return this.value +' cal';
+                    },
+                    style: {
+                        color: '#4572A7'
+                    }
+                },
+            }, { // Secondary yAxis
+                min: 150,
+                max: 170,
+                title: {
+                    text: 'Weight',
+                    style: {
+                        color: '#AA4643'
+                    }
+                }, 
+                labels: {
+                    formatter: function() {
+                        return this.value +' lb';
+                    },
+                    style: {
+                        color: '#AA4643'
+                    }
+                },
+                opposite: true
+            }],
             tooltip: {
                 formatter: function() {
                     return this.y + " Cal.";
@@ -38,9 +70,20 @@ $(function () {
                     }
                 }
             },
-            series: [{
+            series: [{ // Primary yAxis data
+                name: 'Calories',
+                yAxis: 0,
+                type: 'spline',
+                color: '#4572A7',
             	showInLegend: false,
                 data: gon.past_calorie_data[0]
+            },{ // Secondary yAxis data
+                name: 'Weight',
+                yAxis: 1,
+                type: 'spline',
+                color: '#AA4643',
+                showInLegend: false,
+                data: gon.past_weight_data[0]
             }]
         });
     });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,11 @@ class WelcomeController < ApplicationController
     @foods = Food.today.order("created_at desc").all
     @weight = Weight.today.first
     gon.past_calorie_data = Food.past_day_totals(14)
+    gon.past_weight_data = Weight.past_day_totals(14)
   end
 
   def show
+    @weight = Weight.by_day(params[:day].to_i.days.ago).first
     @foods = Food.by_day(params[:day].to_i.days.ago).order("created_at desc").all
   end
 end
